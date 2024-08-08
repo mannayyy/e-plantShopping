cf47726a2b2eb1f5a4b564d221759082acbf94ad
mannayyy <ohsnap.toto@gmail.com>
2024-08-08 18:52:38 -0400

    deployed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/shoppingreact",
-  base: "/e-PlantShopping",
+  base: "/e-plantShopping",
   plugins: [react()],
 })
